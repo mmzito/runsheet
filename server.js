b0811c9ae692412fa3bb79f9cc24abe457d46e17
mmzito <mm.zito@outlook.com>
2026-04-15 09:28:56 +1000

    Back to openid profile email only - offline_access also rejected

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
 const CLIENT_ID = process.env.XERO_CLIENT_ID;
 const CLIENT_SECRET = process.env.XERO_CLIENT_SECRET;
 const REDIRECT_URI = process.env.XERO_REDIRECT_URI || 'http://localhost:3000/callback';
-const SCOPES = 'openid profile email offline_access';
+const SCOPES = 'openid profile email';
 
 let xeroClient = null;
 
